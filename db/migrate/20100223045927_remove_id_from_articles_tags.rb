3532fbdd5727183a05528431a3852b2e8845ad73
Robert Whitener <robwhitener@robert-whiteners-macbook.local>
2011-05-01 22:34:17 -0400

    removing an extraneous migration file

--- db/migrate/20100223045927_remove_id_from_articles_tags.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIdFromArticlesTags < ActiveRecord::Migration
-  def self.up
-    remove_column :articles_tags, :id
-  end
-
-  def self.down
-    add_column :articles_tags, :id, :integer
-  end
-end
