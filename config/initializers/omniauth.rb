e35ce9347e4ce05c7b9374e90691ec899ee319c3
Robert Whitener <robwhitener@robert-whiteners-macbook.local>
2011-05-11 00:19:34 -0400

    oopsie

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, "160343384030567", "75b140df0def2a3932a82380d04f1449", {:client_options => {:ssl => {:ca_file => "/usr/lib/ssl/"}}}
+    provider :facebook, "160343384030567", "75b140df0def2a3932a82380d04f1449", {:client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
 end
