13419776d5aab5cbb099a775e9fc7e414e4ad338
Robert Whitener <robwhitener@robert-whiteners-macbook.local>
2011-05-11 00:41:31 -0400

    still fighting with omniauth and facebook authentication (Heroku)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, "160343384030567", "75b140df0def2a3932a82380d04f1449", {:client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
+    provider :facebook, "160343384030567", "75b140df0def2a3932a82380d04f1449", {:scope => 'email':client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
+end
+
+OmniAuth::Strategies::Facebook.class_eval do 
+  #This monkey patches the oa-oauth gem to reduce the default amount of access your app asks for
+  #The reasoning here is that there is a "strong inverse correlation" between the amount of data you ask
+  # for and the likelihood a user will give you access. Scaring users is not a good idea in Ecommerce
+
+  def request_phase
+    options[:scope] ||= "email"
+    super
+  end      
 end
