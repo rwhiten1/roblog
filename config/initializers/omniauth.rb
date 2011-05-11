63df8d492238cb3be188f3712003bdae47f95a1f
Robert Whitener <robwhitener@robert-whiteners-macbook.local>
2011-05-11 01:07:17 -0400

    trying again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, "160343384030567", "75b140df0def2a3932a82380d04f1449", 
-    {:scope => 'email,offline_access',:client_options => {:ssl => {:ca_file => "/usr/lib/ssl/ca-certificates.crt"}}}
+    {:scope => 'email, offline_access',:client_options => {:ssl => {:ca_path => "/usr/lib/ssl"}}}
 end
 
 #OmniAuth::Strategies::Facebook.class_eval do 
