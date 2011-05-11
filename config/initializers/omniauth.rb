4353f5ca30b639c94524d4ca74fa89a2cbf96d52
Robert Whitener <robwhitener@robert-whiteners-macbook.local>
2011-05-11 01:34:18 -0400

    jalksdjflas

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     puts "IS OMNIAUTH INITIALIZER BEING CALLED?"
-    provider :facebook, "160343384030567", "75b140df0def2a3932a82380d04f1449", 
-    {:scope => 'email, offline_access',:client_options => {:ssl => {:ca_file => "/etc/ssl/certs/ca-certificates.crt"}}}
+    provider :facebook, '160343384030567', '75b140df0def2a3932a82380d04f1449', 
+    {:scope => 'email, offline_access',:client_options => {:ssl => {:ca_file => '/etc/ssl/certs/ca-certificates.crt'}}}
 end
 
 #OmniAuth::Strategies::Facebook.class_eval do 
